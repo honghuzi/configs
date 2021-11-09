7b0907bbb9b1f05a82a0beffc9a069f83267b62d
Chen Yinbo <chenyb.phy@qq.com>
2021-11-09 14:05:03 +0800

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,77 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(load! "+misc")
+(load! "+ui")
+(load! "+myorg")
+(load! "lang/+sql")
+(load! "lang/+python")
+(load! "keybindings/+python")
+(load! "tool/+myvc")
+(load! "tool/+project")
+(load! "tool/+coding.el")
+;; (load! "lisp/everything.el")
 
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(defun +my/better-font()
-  (interactive)
-  ;; english font
-  (if (display-graphic-p)
-      (progn
-        (set-face-attribute 'default nil :font (format "%s:pixelsize=%d" "Fira Code" 17)) ;; 11 13 17 19 23
-        ;; chinese font
-        (dolist (charset '(kana han symbol cjk-misc bopomofo))
-          (set-fontset-font (frame-parameter nil 'font)
-                            charset
-                            (font-spec :family "Microsoft Yahei")))) ;; 14 16 20 22 28
-    ))
-
-(defun +my|init-font(frame)
-  (with-selected-frame frame
-    (if (display-graphic-p)
-        (+my/better-font))))
-
-(if (and (fboundp 'daemonp) (daemonp))
-    (add-hook 'after-make-frame-functions #'+my|init-font)
-  (+my/better-font))
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "V:/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-;; (setq ein:output-area-inlined-images t)
-
-;; (setq python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "--simple-prompt -i")
-(with-eval-after-load 'org
-    (org-babel-do-load-languages
-     'org-babel-load-languages
-     '(
-       ;; (python . t)
-       ;; (R . t)
-       (jupyter . t)
-       )
-     )
-    )
-(setq ob-async-no-async-languages-alist '("jupyter-python"))
-(add-hook! '+org-babel-load-functions
-    (defun +org-babel-load-jupyter-h (lang)
-      (and (string-prefix-p "jupyter-" (symbol-name lang))
-       (require 'ob-jupyter)
-           (require lang nil t))))
+;; (setq url-proxy-services '(("https" . "E905819:@172.21.1.30:8080")))
+;; sql
+;; (define-key sql-mode-map (kbd "C-c C-f") 'sqlformat-buffer)
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
