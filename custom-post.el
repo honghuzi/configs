b863724c123b90fc1625a0f9667cc891d9c42a6b
Chen Yinbo <chenphysics70@gmail.com>
2023-04-24 09:42:45 +0800

    Update custom-post.el

--- custom-post.el
+++ custom-post.el
@@ -1,10 +1,4 @@
-
-(use-package simple-httpd)
-
-(setq org-roam-db-location "d:/docs/org/org-roam.db")
-(setq org-roam-directory "d:/docs/org/org-roam/")
-(setq org-directory "d:/docs/org")
-(setq org-projectile-file "TODOs.org");; disable packages
+;; disable packages
 
 ;; org mode 中默认不开启 grip mode
 ;; (add-hook 'org-mode-hook
@@ -14,9 +8,76 @@
 ;;
 ;; (define-key org-mode-map (kbd "C-c C-g") nil)
 
-;; prog-mode
+;; eaf
 
+;; 在 windows 下面都有更好的方案，pdf 用 zotero 或 Sumatra PDF, mindmap 用 draw.io
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-application-framework/")
+;; (require 'eaf)
+
+;; (require 'eaf-mindmap)
+;; (require 'eaf-pdf-viewer)
+
+;; remote editing
+(setq tramp-default-method "plink")
+
+
+
+;; (defun my-org-screenshot ()
+;;   "Take a screenshot into a time stamped unique-named file in the
+;;    same directory as the org-buffer and insert a link to this file."
+;;   (interactive)
+;;   (setq filename
+;;         (concat
+;;          (make-temp-name
+;;           (concat (buffer-file-name)
+;;                   "_"
+;;                   (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+;;   (shell-command "snippingtool /clip")
+;;   (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+;;   (insert (concat "[[file:" filename "]]"))
+;;   (org-display-inline-images))
+
+;; lsp-bridge
+
+;; (add-to-list 'load-path "~/.emacs.d/site-lisp/lsp-bridge")
+
+;; (require 'yasnippet)
+;; (yas-global-mode 1)
+
+;; (require 'lsp-bridge)
+;; (setq lsp-bridge-python-command "D:/Program Files/python311/python.exe")
+;; (setq python-shell-exec-path "D:/Program Files/python311")
+
+;; (global-lsp-bridge-mode)
+
+;; ;; prog-mode
+
+(setq mermaid-flags "-s 1.5")
 ;;python
+
+(setq python-prettify-symbols-alist
+      '(("lambda" . "λ")
+        ("->" . "→")
+        ("!=" . "≠")
+        ("==" . "≡")
+        ("<=" . "≤")
+        (">=" . "≥")
+        ;; ("!" . "￢")
+        ("null" . "∅")
+        ("def" . "ƒ")
+        ("and" . "∧")
+        ("or" . "∨")
+        ("in" . "∈")
+        ("not in" . "∉")
+        ("for" . "∀")
+        ("return" . "⇨")
+        ("yield" . "⇦")
+        (" ** 2" . "²")
+        ("np.sqrt" . "√")
+        ("int" . "ℤ")
+        ("float" . "ℝ")
+        ;; ("bool" . "𝔹")
+        ))
 
 (setq python-shell-interpreter "D:\\Program Files\\python311\\python.exe")
 (setq lsp-pyright-python-executable-cmd "D:\\Program Files\\python311\\python.exe")
@@ -26,8 +87,18 @@
 ;; julia
 (setq lsp-julia-package-dir nil)
 (setq lsp-julia-flags `("-JD:/Program Files/julials/languageserver.so"))
-(setq lsp-julia-default-depot "c:/Users/ccc/.julia/")
-(setq lsp-julia-default-environment "c:/Users/ccc/.julia/environments/v1.9/")
+(setq lsp-julia-default-depot "~/.julia/")
+(setq lsp-julia-default-environment "~/.julia/environments/v1.9/")
+
+;;sql
+(use-package sqlformat
+  :config
+  (setq sqlformat-command 'sqlformat)
+  (setq sqlformat-args '("-kupper" "--indent_columns" "-s" "-r"))
+  (define-key sql-mode-map (kbd "C-c C-f") 'sqlformat-buffer)
+  (add-hook 'sql-mode-hook 'sqlformat-on-save-mode)
+  )
+
 
 ;; org mode and emacs-jupyter
 (use-package zmq)
@@ -41,17 +112,31 @@
 (setq org-projectile-file "TODOs.org")
 (setq centaur-org-directory org-directory)
 
+
+(setq org-download-screenshot-method "convert clipboard: %s")
+(setq-default org-download-image-dir "./images")
+(setq-default org-download-heading-lvl nil)
+(setq org-download-method 'directory)
+
 (use-package org
   :config
   (unbind-key "C-c C-g" org-mode-map)
   (require 'org-download)
   (add-hook 'dired-mode-hook 'org-download-enable)
+  (setq org-download-screenshot-method "convert clipboard: %s")
+  (setq-default org-download-image-dir "./images")
+  (setq-default org-download-heading-lvl nil)
+  (setq org-download-method 'directory)
+  ;; org inline images
+  (setq org-image-actual-width nil)
+
   (org-babel-do-load-languages
    'org-babel-load-languages
    '(
      (emacs-lisp . t)
      (python . t)
      (julia . t)
+     (mermaid . t)
      (jupyter . t)
      )
    )
@@ -82,47 +167,11 @@
 
   :after (:all org)
   )
+
 
 ;; org-dowload, support paste images to org-mode
 
 (use-package org-download)
-
-(setq centaur-org-directory org-directory)
-
-(use-package org
-  :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '(
-     (emacs-lisp . t)
-     (python . t)
-     (julia . t)
-     (jupyter . t)
-     )
-   )
-  (setq org-src-fontify-natively t
-        org-src-window-setup 'current-window ;; edit in current window
-        org-src-strip-leading-and-trailing-blank-lines t
-        org-src-preserve-indentation t ;; do not put two spaces on the left
-        org-src-tab-acts-natively t
-        org-edit-src-content-indentation 0
-	    org-confirm-babel-evaluate nil)
-  (setq org-confirm-babel-evaluate nil)
-  (setq org-superstar-headline-bullets-list '(?■ ?◆ ?▲ ?▶))
-
-  )
-
-
-(use-package jupyter
-  :defer t
-  :init
-  (setq org-babel-default-header-args:jupyter-python '((:async . "yes")
-                                                       (:session . "py")
-                                                       (:kernel . "python3")))
-  (setq org-babel-default-header-args:jupyter-julia '((:async . "yes")
-                                                      (:session . "jl")
-                                                      (:kernel . "julia-1.8")))
-  (setq ob-async-no-async-languages-alist '("python" "jupyter-python" "julia" "jupyter-julia"))
-
-  :after (:all org)
+(use-package zotxt
+  :ensure t
   )
